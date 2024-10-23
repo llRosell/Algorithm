b49f602aea66041d496b6a0143edab2d1646ee5f
llRosell <llovein0920@gmail.com>
2024-10-23 18:13:31 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,12 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
+        double sum = 0;
         
         for(int i = 0; i < arr.length; i++){
-            answer += arr[i];
+            sum += arr[i];
         }
-        answer = answer/arr.length;
+        answer = sum/arr.length;
         return answer;
     }
 }
