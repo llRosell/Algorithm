1cd073b3d99dc0887c2caadd4f78f7a0bd1dabb1
llRosell <llovein0920@gmail.com>
2024-10-25 18:42:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while(true){
+            answer+=n%10;
+            
+            if(n<10) break;
+            
+            n=n/10;
+        }
+        return answer;
+    }
+}
